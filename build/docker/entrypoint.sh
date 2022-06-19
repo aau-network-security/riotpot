bbca3740210c26cb678ad50363232466b66cefaa
Ricardo Yaben <32867697+RicYaben@users.noreply.github.com>
2022-06-19 19:31:59 +0200

    initial

--- build/docker/entrypoint.sh
+++ build/docker/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-
-# wait for the database to be up
+# Wait for the database to be up
 if [ $DB_HOST ]
 then
-  echo "Waiting for mongodb..."
+  echo "Waiting for database..."
 
   while ! nc -z $DB_HOST $DB_PORT; do
     sleep 0.1
   done
 
-  echo "MongoDB started"
+  echo "database started"
 fi
 
 # run riotpot
