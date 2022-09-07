7f04b2fbaa669fcbcf0119dff1b1a35544b3a432
Ricardo Yaben <32867697+RicYaben@users.noreply.github.com>
2022-09-07 21:11:15 +0200

    Added missing file

---
+++ cmd/riotpot/flags.go
@@ -0,0 +1,81 @@
+/*
+This package determines the flags set in the environment
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/riotpot/api"
+	"github.com/riotpot/api/proxy"
+	"github.com/riotpot/api/service"
+	"github.com/riotpot/internal/globals"
+	"github.com/rs/zerolog"
+)
+
+type Routers []api.Router
+
+var (
+	// Routers of the application
+	routers Routers = Routers{
+		// Proxy router
+		proxy.ProxiesRouter,
+		// Services router
+		service.ServicesRouter,
+	}
+)
+
+var (
+	debug  = flag.Bool("debug", true, "Set log level to debug")
+	runApi = flag.Bool("api", true, "Whether to start the API")
+)
+
+func setupApi() *gin.Engine {
+	// Create a router
+	router := gin.Default()
+
+	// - PUT and PATCH methods
+	// - Origin header
+	// - Credentials share
+	// - Preflight requests cached for 12 hours
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1"}, // TODO: Change this to wherever the front-end is located!
+		AllowMethods:     []string{"OPTIONS", "PUT", "PATCH", "GET", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	root := router.Group(globals.ApiEndpoint)
+
+	// Add the proxy routes
+	for _, router := range routers {
+		router.AddToGroup(root)
+	}
+
+	// Serve the Swagger UI files in the root of the api
+	// TODO: [7/24/2022] Use Pakr or Statik to bundle non-golang files into the binary
+	root.Static("swagger", "../../api/swagger")
+
+	return router
+}
+
+func ParseFlags() {
+	// Set the logging level to debug
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	// Starts the API
+	if *runApi {
+		// Serve the API
+		api := setupApi()
+		api.Run(fmt.Sprintf("%s:%s", globals.ApiHost, globals.ApiPort))
+	}
+}
