a8127ce74be57a24e09b944e71ebba15a9e1064c
Ricardo Yaben <32867697+RicYaben@users.noreply.github.com>
2023-01-22 11:58:49 +0100

    Replaced `react-scripts` with `Vite`

---
+++ ui/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        // this is required for the SCSS modules
+        find: /^~(.*)$/,
+        replacement: "$1",
+      },
+    ],
+  },
+  build: {
+    outDir: "build",
+  },
+});
